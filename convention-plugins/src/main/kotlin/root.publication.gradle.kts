17928e2f74845eb1517c4d340766b6731004c183
Niels Simonides <niels.simonides@ing.com>
2024-04-24 15:42:10 +0200

    Rename environment variables + snapshot suffix

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
     group = "community.flock.aigentic"
-    version = "0.0.1"
+    version = "0.0.1-SNAPSHOT"
 }
 
 nexusPublishing {
@@ -14,8 +14,8 @@ nexusPublishing {
         sonatype {  //only for users registered in Sonatype after 24 Feb 2021
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username.set(System.getenv("ORG_GRADLE_PROJECT_sonatypeUsername"))
-            password.set(System.getenv("ORG_GRADLE_PROJECT_sonatypePassword"))
+            username.set(System.getenv("SONATYPE_USERNAME"))
+            password.set(System.getenv("SONATYPE_PASSWORD"))
         }
     }
 }
