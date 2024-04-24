26174c331be7d5eac042013275f90595f6be2351
Niels Simonides <niels.simonides@ing.com>
2024-04-24 13:45:36 +0200

    Configure username and password differently

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -14,6 +14,8 @@ nexusPublishing {
         sonatype {  //only for users registered in Sonatype after 24 Feb 2021
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("ORG_GRADLE_PROJECT_sonatypeUsername"))
+            password.set(System.getenv("ORG_GRADLE_PROJECT_sonatypePassword"))
         }
     }
 }
