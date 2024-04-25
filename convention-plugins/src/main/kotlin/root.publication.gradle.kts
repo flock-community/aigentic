9170617de812d1be9826c633a59360cab9367990
Niels Simonides <niels.simonides@ing.com>
2024-04-25 14:50:41 +0200

    Revert to how it should work

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -4,20 +4,23 @@ plugins {
 
 allprojects {
     group = "community.flock.aigentic"
-    version = "0.0.1"
+    version = "0.0.2-SNAPSHOT"
 }
 
 nexusPublishing {
-//    repositoryDescription.set("community.flock.aigentic:0.0.1")
-    useStaging.set(false)
+
+//    val isSnapshot = version.toString().endsWith("SNAPSHOT")
+//    val url = if (isSnapshot) "https://s01.oss.sonatype.org/content/repositories/snapshots/" else "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+
+//    useStaging.set(isSnapshot)
     // Configure maven central repository
     // https://github.com/gradle-nexus/publish-plugin#publishing-to-maven-central-via-sonatype-ossrh
     repositories {
         sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-//            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
-//            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+//            nexusUrl.set(uri(url))
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
+//            snapshotRepositoryUrl.set(uri(url))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
             username.set(System.getenv("SONATYPE_USERNAME"))
             password.set(System.getenv("SONATYPE_PASSWORD"))
         }
