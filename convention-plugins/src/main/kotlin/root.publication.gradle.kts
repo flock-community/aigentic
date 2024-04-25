3ad942f14da2379e7ea4036fe09baba1872ec1c2
Niels Simonides <niels.simonides@ing.com>
2024-04-25 19:56:44 +0200

    Try with fixed urls

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -4,21 +4,14 @@ plugins {
 
 allprojects {
     group = "community.flock.aigentic"
-    version = "0.0.2"
+    version = "0.0.3-SNAPSHOT"
 }
 
 nexusPublishing {
-
-    val isSnapshot = version.toString().endsWith("-SNAPSHOT")
-    val url = if (isSnapshot) "https://s01.oss.sonatype.org/content/repositories/snapshots/" else "https://s01.oss.sonatype.org/service/local/"
-
-
-//    if(!isSnapshot) useStaging.set(false)
-
     repositories {
-        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
-            nexusUrl.set(uri(url))
-            snapshotRepositoryUrl.set(uri(url))
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
             username.set(System.getenv("SONATYPE_USERNAME"))
             password.set(System.getenv("SONATYPE_PASSWORD"))
         }
