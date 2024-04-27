2ceb28bd44338459656a39a1fb1dcbce89be3659
Niels Simonides <5821618+nsmnds@users.noreply.github.com>
2024-04-27 14:50:49 +0200

    Setup Gradle project (#1)
    * Setup gradle project
    
    * Added github workflows
    
    * Updated build targets
    
    * Test failing test
    
    * Pass test again
    
    * Set key passphrase and remove key id
    
    * Added keyName again
    
    * Updated sonatype url
    
    * try different task
    
    * Set sonatype username and password environment variables
    
    * Configure username and password differently
    
    * Rename environment variables + snapshot suffix
    
    * Spaces
    
    * Spaces
    
    * Try without passphrase
    
    * Enable targets again
    
    * Enable password again
    
    * Remove password
    
    * Without keyname
    
    * Sign
    
    * Try release
    
    * Try without snapshot
    
    * With snapshot
    
    * Gradle debug
    
    * Disable configuration cache
    
    * Remove debug
    
    * Try different tasks
    
    * Try repo description
    
    * Try repo description with version
    
    * Try useStaging
    
    * Different tasks
    
    * Revert to how it should work
    
    * Try without snapshot
    
    * set usestaging based on if snapshot
    
    * Try different url
    
    * Try without snapshot
    
    * Remove space
    
    * Try with fixed urls
    
    * Set version number
    
    * Changed GH workflow triggers
    
    * Setup spotless and dokka
    
    * Change name
    
    * Set dokka output dir
    
    * Disable configuration cache
    
    * Added badges
    
    * Fix license link
    
    * Disable configuration cache for build
    
    * Disable configuration cache for spotless
    
    * Added js target
    
    * Added CODEOWNERS
    
    ---------
    
    Co-authored-by: Niels Simonides <niels.simonides@ing.com>

---
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("io.github.gradle-nexus.publish-plugin")
+}
+
+allprojects {
+    group = "community.flock.aigentic"
+    version = "0.0.4-SNAPSHOT"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("SONATYPE_USERNAME"))
+            password.set(System.getenv("SONATYPE_PASSWORD"))
+        }
+    }
+}
