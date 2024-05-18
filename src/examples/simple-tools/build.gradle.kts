b619c8063c56ad1ea78d94ad53da29d173f7c266
Niels Simonides <niels.simonides@dhl.com>
2024-05-18 14:01:47 +0200

    Moved example

---
+++ src/examples/simple-tools/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+}
+
+kotlin {
+    jvm()
+    js(IR) {
+        nodejs()
+        generateTypeScriptDefinitions()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(project(":src:core"))
+                implementation(project(":src:providers:openai"))
+                implementation(project(":src:tools:openapi"))
+                implementation ("ch.qos.logback:logback-classic:1.2.3")
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                // CIO is for JVM, Android, Native. For other platforms pick the correct engine: https://ktor.io/docs/client-engines.html#platforms
+                implementation(libs.ktor.client.cio)
+            }
+        }
+    }
+}
---
+++ src/examples/simple-tools/src/jvmMain/kotlin/community/flock/aigentic/example/AdministrativeAgentExampleJvm.kt
@@ -0,0 +1,13 @@
+package community.flock.aigentic.example
+
+import kotlinx.coroutines.runBlocking
+
+private val openAIAPIKey =
+    System.getenv("OPENAI_KEY").also {
+        if (it.isNullOrEmpty()) error("Set 'OPENAI_KEY' environment variable!")
+    }
+
+fun main(): Unit =
+    runBlocking {
+        runAdministrativeAgentExample(openAIAPIKey)
+    }
