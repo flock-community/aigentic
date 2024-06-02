70620a60aaa5fc47bb0347ae7c2d3e72f1979ff7
Niels Simonides <5821618+nsmnds@users.noreply.github.com>
2024-06-02 10:11:02 +0200

    Publish json schema (#15)
    * Update build.gradle.kts
    
    * Version bump

--- src/providers/jsonschema/build.gradle.kts
+++ src/providers/jsonschema/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.kotlinx.serialization)
     alias(libs.plugins.kotest.multiplatform)
+    id("module.publication")
 }
 
 kotlin {
